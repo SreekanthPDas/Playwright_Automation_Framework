c1f67577cf9c5b81914d036ee14968794f668267
SreekanthPDas <sreekanthpdas89@gmail.com>
2026-04-12 22:18:15 +0530

    Update to playwright.config and playwright.yaml
    playwright.config.ts update to set headLess=true
    playwright.yaml update to set ENV=QA

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,// || 'https://opensource-demo.orangehrmlive.com',
     /* Run in Headed mode. */
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     /*Collect screenshots and videos when test fails and retrying the failed test. See https://playwright.dev/docs/video#record-video-only-on-failure */
